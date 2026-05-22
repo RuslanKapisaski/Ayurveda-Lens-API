49ad70768d3f15194f8553ddd2568cebec36389f
Руслан Каписъски <ruslank1@abv.bg>
2026-05-22 13:48:32 +0300

    Аdd lazy loading for ML model to prevent memory crash and lock for concurrent requests

--- app/model/classifier.py
+++ app/model/classifier.py
@@ -1,6 +1,7 @@
 from transformers import pipeline
 import json
 import os
+from threading import Lock
 
 # Load dosha map
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -9,20 +10,30 @@ DOSHA_MAP_PATH = os.path.join(BASE_DIR, '../data/dosha_map.json')
 with open(DOSHA_MAP_PATH, 'r') as f:
     dosha_map = json.load(f)
 
-# Food101 pretrained model
-classifier = pipeline(
-    task="image-classification",
-    model="nateraw/food"
-)
+# Food101 pretrained model with lazy loading
+classifier = None
+lock = Lock()
+
+def get_classifier():
+    global classifier
+    if classifier is None:
+        with lock:
+            print("Loading model...")  # за debug
+            classifier = pipeline(
+                task="image-classification",
+                model="nateraw/food"
+            )
+    return classifier
 
 def classify_image(tensor):
-    results = classifier(tensor,top_k=1)
+    model = get_classifier()
+    results = model(tensor, top_k=1)
     top_result = results[0]
 
-    food_name = top_result['label'].lower().replace("_"," ")
-    confidence = round(top_result['score']*100, 2)
+    food_name = top_result['label'].lower().replace("_", " ")
+    confidence = round(top_result['score'] * 100, 2)
 
-    dosha = dosha_map.get(food_name,{
+    dosha = dosha_map.get(food_name, {
         "vata": "neutral",
         "pitta": "neutral",
         "kapha": "neutral",
